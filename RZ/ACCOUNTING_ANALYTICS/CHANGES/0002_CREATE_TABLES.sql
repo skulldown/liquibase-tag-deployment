16d7b51dc0f8e2241af07286710d7de4b244da54
skulldown <yogesh.makwana@outlook.com>
2025-06-24 16:10:30 +0530

    added code from main branch

--- RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset 12:teacher
+--changeset 12:teacher_11
 
 use database DEV_RZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.teacher(
+CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.teacher_11(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
