d3e52124a1c226f95f6b450a7e4e5e4199ee9495
skulldown <yogesh.makwana@outlook.com>
2025-07-21 19:39:16 +0530

    added tag

--- RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset 12:student9
+--changeset 12:student10
 
 use database DEV_RZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.student_8(
+CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.student_10(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
@@ -18,12 +18,6 @@ CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.student_8(
 
 
 
--- CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.student_11948sd9112(
---     ID             NUMBER       PRIMARY KEY,
---     NAME           STRING       NOT NULL,
---     STATUS         STRING,
---     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
--- );
-
-
+--changeset 12:tag_student10
+--comment: Tag deployment
 --tag: liquibase-dev-deploy-123
