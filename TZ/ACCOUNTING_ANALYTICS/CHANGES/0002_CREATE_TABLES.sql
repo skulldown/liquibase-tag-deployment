08fe447872ddab703b7dc21a0dfa36eb37f877cb
skulldown <yogesh.makwana@outlook.com>
2025-07-21 11:50:08 +0530

    added module abse tag

---
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -0,0 +1,32 @@
+--liquibase formatted sql
+
+--changeset 12:teacher_11empp123
+
+use database DEV_TZ;
+use schema ACCOUNTING_ANALYTICS;
+use role FULL_ACCESS_ROLE;
+use warehouse SNOWFLAKE_LEARNING_WH;
+
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.teacher112(
+    ID             NUMBER       PRIMARY KEY,
+    NAME           STRING       NOT NULL,
+    STATUS         STRING,
+    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.student_12(
+    ID             NUMBER       PRIMARY KEY,
+    NAME           STRING       NOT NULL,
+    STATUS         STRING,
+    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.emmp12(
+--     ID             NUMBER       PRIMARY KEY,
+--     NAME           STRING       NOT NULL,
+--     STATUS         STRING,
+--     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+-- );
