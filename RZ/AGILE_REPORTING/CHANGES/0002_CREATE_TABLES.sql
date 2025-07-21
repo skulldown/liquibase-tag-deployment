14d7433a54212b26d951587bb5787ff394cf4dde
skulldown <yogesh.makwana@outlook.com>
2025-07-21 15:15:42 +0530

    aded

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-empp1010
+--changeset DEMO:0010-empp10101
 use database DEV_RZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
@@ -8,7 +8,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.teacher_1010(
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.teacher_10101(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
