982f29e817223815233265b3a24542d8ad080339
skulldown <yogesh.makwana@outlook.com>
2025-07-21 15:41:48 +0530

    aded

--- TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset 12:empp37
+--changeset 12:empp38
 
 use database DEV_TZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.empp37(
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.empp38(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
--- TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-empp37
+--changeset DEMO:0010-empp38
 use database DEV_TZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
@@ -8,7 +8,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.empp37(
+CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.empp38(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
