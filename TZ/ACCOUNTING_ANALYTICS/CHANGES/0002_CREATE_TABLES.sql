63eab6762a876f39b579c1421af1a7507cb695e6
skulldown <yogesh.makwana@outlook.com>
2025-07-21 14:59:08 +0530

    aded

--- TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,23 +1,16 @@
 --liquibase formatted sql
 
---changeset 12:teacher_11empp123
+--changeset 12:
 
 use database DEV_TZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.teacher112(
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.teacher(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.emmp12(
-    ID             NUMBER       PRIMARY KEY,
-    NAME           STRING       NOT NULL,
-    STATUS         STRING,
-    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-);
