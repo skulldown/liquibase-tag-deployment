6373bc0b755a5285411057f5654c419b5cff48a5
skulldown <yogesh.makwana@outlook.com>
2025-07-22 11:26:16 +0530

    asshdghgasdf

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-student_8 labels:labale_rz
+--changeset DEMO:0010-student_9 labels:labale_rz
 --tagDatabase: 'DEV_RZ_22_july'
 use database DEV_RZ;
 use schema AGILE_REPORTING;
@@ -10,7 +10,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.student_8(
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.student_9(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
