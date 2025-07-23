cc3a3e70e823d0fa278edd9581f24aee38983844
skulldown <yogesh.makwana@outlook.com>
2025-07-23 11:38:41 +0530

    adedd

---
+++ RZ/AGILE_REPORTING/bin/CHANGES/0002_CREATE_TABLES.sql
@@ -0,0 +1,38 @@
+--liquibase formatted sql
+
+--changeset DEMO:0010-student_18 labels:${liquibase.label}
+
+use database DEV_RZ;
+use schema AGILE_REPORTING;
+use role FULL_ACCESS_ROLE;
+use warehouse SNOWFLAKE_LEARNING_WH;
+
+
+
+
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.student_17(
+    ID             NUMBER       PRIMARY KEY,
+    NAME           STRING       NOT NULL,
+    STATUS         STRING,
+    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+-- CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.empp382asasdsgh(
+--     ID             NUMBER       PRIMARY KEY,
+--     NAME           STRING       NOT NULL,
+--     STATUS         STRING,
+--     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+-- );
+
+
+-- CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.empp382dsghasas(
+--     ID             NUMBER       PRIMARY KEY,
+--     NAME           STRING       NOT NULL,
+--     STATUS         STRING,
+--     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+-- );
+
+
