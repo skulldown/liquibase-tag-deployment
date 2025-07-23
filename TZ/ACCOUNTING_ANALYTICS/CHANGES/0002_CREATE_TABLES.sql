71ede54e56479688043ddda34736d17172b386c3
skulldown <yogesh.makwana@outlook.com>
2025-07-23 14:48:44 +0530

    adedd

--- TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset 12:demod231
+--changeset 12:demod2315
 
 use database DEV_TZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.demod243(
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.demod2435(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
