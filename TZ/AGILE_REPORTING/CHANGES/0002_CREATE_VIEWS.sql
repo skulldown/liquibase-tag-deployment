e57dd46ea2b4458d2055a21824ccf1228bb6287f
skulldown <yogesh.makwana@outlook.com>
2025-07-23 12:37:32 +0530

    adedd

--- TZ/AGILE_REPORTING/CHANGES/0002_CREATE_VIEWS.sql
+++ TZ/AGILE_REPORTING/CHANGES/0002_CREATE_VIEWS.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-demod23
+--changeset DEMO:0010-demod2
 use database DEV_TZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.demod23(
+CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.demod2(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
