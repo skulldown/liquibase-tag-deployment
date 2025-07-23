ed28b199ab2c74f7de2e69ea40e90bc98263d32a
skulldown <yogesh.makwana@outlook.com>
2025-07-23 18:33:54 +0530

    added latest code_ python tag add

--- TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:teacher
+--changeset DEMO:teacher_1
 use database DEV_TZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
@@ -8,7 +8,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.teacher(
+CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.teacher_1(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
