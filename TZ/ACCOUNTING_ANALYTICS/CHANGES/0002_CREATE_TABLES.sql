99aa8423407c0c8f70bfad5f94e898d56841c5bd
skulldown <yogesh.makwana@outlook.com>
2025-07-23 18:27:37 +0530

    added latest code_ python tag add

--- TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,20 +1,15 @@
 --liquibase formatted sql
 
---changeset 12:demod231579
+--changeset 12:Student
 
 use database DEV_TZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.demod243579(
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.Student(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
-
-
-
-
-
