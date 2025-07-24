8f9acb110ae6ff09e2c76271e53fd165e9d3ec82
skulldown <yogesh.makwana@outlook.com>
2025-07-24 15:38:53 +0530

    Added workflow to liqubase

---
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES_data.sql
@@ -0,0 +1,27 @@
+--liquibase formatted sql
+
+--changeset DEMO:0010-student_3
+
+use database DEV_RZ;
+use schema AGILE_REPORTING;
+use role FULL_ACCESS_ROLE;
+use warehouse SNOWFLAKE_LEARNING_WH;
+
+
+
+CREATE TABLE IF NOT EXISTS DEV_TZ.ACCOUNTING_ANALYTICS.Student_3(
+    ID             NUMBER       PRIMARY KEY,
+    NAME           STRING       NOT NULL,
+    STATUS         STRING,
+    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+
+
+
+
+
+
