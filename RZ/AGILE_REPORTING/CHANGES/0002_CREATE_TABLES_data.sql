0fc8410573c656653d9d75c60f3ab230c865ee00
skulldown <yogesh.makwana@outlook.com>
2025-07-24 15:52:19 +0530

    Added workflow to liqubase

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES_data.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES_data.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-student_4
+--changeset DEMO:0010-student_9
 
 use database DEV_RZ;
 use schema AGILE_REPORTING;
@@ -9,7 +9,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.Student_3(
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.teacher(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
