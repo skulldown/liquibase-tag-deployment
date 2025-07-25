6617254b6aebfe993e981e7b209768131fd0b7f4
skulldown <yogesh.makwana@outlook.com>
2025-07-25 16:59:45 +0530

    Added: latest code]

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:Student_215
+--changeset DEMO:Student_113
 use database DEV_RZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
@@ -8,7 +8,7 @@ use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.Student_215(
+CREATE  DEV_RZ.AGILE_REPORTING.Student_113(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
