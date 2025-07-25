7fe4d6cccb947adee0f3da5da4a8a807fc1b1e34
skulldown <yogesh.makwana@outlook.com>
2025-07-25 15:25:53 +0530

    Added: latest code]

--- TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,25 +1,25 @@
 --liquibase formatted sql
 
 --changeset DEMO:Student_3
-use database DEV_TZ;
-use schema AGILE_REPORTING;
-use role FULL_ACCESS_ROLE;
-use warehouse SNOWFLAKE_LEARNING_WH;
+-- use database DEV_TZ;
+-- use schema AGILE_REPORTING;
+-- use role FULL_ACCESS_ROLE;
+-- use warehouse SNOWFLAKE_LEARNING_WH;
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.Student_1(
-    ID             NUMBER       PRIMARY KEY,
-    NAME           STRING       NOT NULL,
-    STATUS         STRING,
-    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.Student_1(
+--     ID             NUMBER       PRIMARY KEY,
+--     NAME           STRING       NOT NULL,
+--     STATUS         STRING,
+--     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+-- );
 
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.Student_3(
-    ID             NUMBER       PRIMARY KEY,
-    NAME           STRING       NOT NULL,
-    STATUS         STRING,
-    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.Student_3(
+--     ID             NUMBER       PRIMARY KEY,
+--     NAME           STRING       NOT NULL,
+--     STATUS         STRING,
+--     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+-- );
--- TZ/AGILE_REPORTING/CHANGES/0002_CREATE_VIEWS.sql
+++ TZ/AGILE_REPORTING/CHANGES/0002_CREATE_VIEWS.sql
@@ -1,20 +1,13 @@
 --liquibase formatted sql
 
---changeset DEMO:student_7
+--changeset DEMO:student_9
 use database DEV_TZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
 
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.student_1(
-    ID             NUMBER       PRIMARY KEY,
-    NAME           STRING       NOT NULL,
-    STATUS         STRING,
-    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.student_7(
+CREATE TABLE IF NOT EXISTS DEV_TZ.AGILE_REPORTING.student_9(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
