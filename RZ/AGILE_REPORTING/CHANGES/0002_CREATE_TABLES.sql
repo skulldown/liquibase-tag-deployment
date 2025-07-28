9ee114471ccc282e4481e4336d73c2a276768012
skulldown <yogesh.makwana@outlook.com>
2025-07-28 10:45:20 +0530

    Added: latest code]

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:Student_113
+--changeset DEMO:Student_114
 use database DEV_RZ;
 use schema AGILE_REPORTING;
 use role FULL_ACCESS_ROLE;
@@ -15,4 +15,11 @@ CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.Student_113(
     CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.Student_114(
+    ID             NUMBER       PRIMARY KEY,
+    NAME           STRING       NOT NULL,
+    STATUS         STRING,
+    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+);
 
