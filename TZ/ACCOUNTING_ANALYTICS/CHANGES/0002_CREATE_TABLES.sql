b16309d70d33b5a40dad1b3f6eeb7e737f7461b4
skulldown <yogesh.makwana@outlook.com>
2025-07-28 11:01:22 +0530

    Added: latest code]

--- TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ TZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,22 +1,13 @@
 --liquibase formatted sql
 
---changeset 12:Student_21
+--changeset 12:1asdasd
 
 use database DEV_TZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE  DEV_TZ.ACCOUNTING_ANALYTICS.Student_20(
-    ID             NUMBER       PRIMARY KEY,
-    NAME           STRING       NOT NULL,
-    STATUS         STRING,
-    CREATED_AT     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-);
-
-
-
-CREATE  DEV_TZ.ACCOUNTING_ANALYTICS.Student_21(
+CREATE DEV_TZ.ACCOUNTING_ANALYTICS.Student_20(
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
